7cf65a466cf92377da3640fd93b8a68da80a24a6
RyanC <ryan.chengwy@gmail.com>
2015-12-25 14:16:59 +0800

    Initial Commit

---
+++ midterm.film/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  midterm.film
+//
+//  Created by Ryan on 25/12/2015.
+//  Copyright © 2015 Ryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ midterm.filmTests/midterm_filmTests.swift
@@ -0,0 +1,36 @@
+//
+//  midterm_filmTests.swift
+//  midterm.filmTests
+//
+//  Created by Ryan on 25/12/2015.
+//  Copyright © 2015 Ryan. All rights reserved.
+//
+
+import XCTest
+@testable import midterm_film
+
+class midterm_filmTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
