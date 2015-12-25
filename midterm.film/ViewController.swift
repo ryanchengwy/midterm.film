31fc5f3b80489ee67c1226213c5ec4f11813beaf
RyanC <ryan.chengwy@gmail.com>
2015-12-25 19:47:27 +0800

    finished

--- midterm.film/ViewController.swift
+++ midterm.film/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputSignatureTextField: UITextField!
+    @IBOutlet weak var signature: UIButton!
+    @IBOutlet weak var stringSignatureName: UILabel!
+    @IBOutlet weak var stringWarning: UILabel!
+    var signatureArray = [String]()
+    var warningString = ""
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        stringSignatureName.text = ""
+        stringWarning.text = ""
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,40 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func addSignatureButtonPressed(sender: AnyObject) {
+        stringWarning.text = ""
+        checkBlankInput()
+        printSignature()
+        inputSignatureTextField.text = ""
+        
+    }
+    func setSignatureName () {
+        if signatureArray.count < 3{
+        signatureArray.append(inputSignatureTextField.text!)
+        } else {
+            warningString = "只能簽3筆"
+            stringWarning.text = warningString
+        }
+    }
+    func printSignature () {
+        switch signatureArray.count {
+        case 1 :
+            stringSignatureName.text = signatureArray[0]
+        case 2 :
+            stringSignatureName.text = signatureArray[0] + "\n" + signatureArray[1]
+        case 3 :
+            stringSignatureName.text = signatureArray[0] + "\n" + signatureArray[1] + "\n" + signatureArray[2]
+        default :
+            break
+        }
+    }
+    func checkBlankInput () {
+        if inputSignatureTextField.text == "" {
+            warningString = "名字不能留白"
+            stringWarning.text = warningString
+        } else {
+            setSignatureName()
+        }
+    }
 }
 
